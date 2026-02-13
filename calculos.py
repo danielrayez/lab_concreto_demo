44be3b0fbddec963e4982aad3ffbec36c9e7de91
Daniel Ramirez <ingdanielrayez@gmail.com>
2026-02-13 16:34:34 -0500

    agrego factor para corregir resistencia por relacion l/d

--- calculos.py
+++ calculos.py
@@ -3,6 +3,17 @@
 # ========================================
 import math
 
+def area_cilindro(diametro_mm):
+
+    """
+    Calcula el área de la sección transversal del cilindro.
+    
+    Parámetros:
+    - diametro_mm: Diámetro del cilindro en mm
+    """
+    radio_mm = diametro_mm / 2
+    area_mm2 = math.pi * (radio_mm ** 2)
+    return area_mm2
 
 
 def volumen_cilindro(diametro_mm, altura_mm):
@@ -25,12 +36,12 @@ def densidad_cilindro(volumen_m3, peso_kg):
     Parámetros:
     - volumen_m3: Volumen del cilindro en m³
     - peso_kg: Peso del cilindro en kg
+    - densidad en kg/m³ al 10 kg/m³ más cercano
     """
-    if volumen_m3 > 0:
-        return round(peso_kg / volumen_m3, 2)
-    else:
-        return 0.0
-    
+
+    if volumen_m3 > 0 and peso_kg > 0:
+        densidad = round(peso_kg / volumen_m3, 2)
+        return round(densidad / 10) * 10
 
 def resistencia_compresion(carga_kn, diametro_mm):
     """
@@ -54,4 +65,4 @@ def evolucion_resistencia(fc, resistencia):
     if fc > 0:
         return round((resistencia / fc) * 100, 2)
     else:
-        return 0.0
+        return  0.0
--- ensayo_compresion.py
+++ ensayo_compresion.py
@@ -1,18 +1,42 @@
 import streamlit as st
 import pandas as pd
 from datetime import date
-from calculos import resistencia_compresion, evolucion_resistencia , volumen_cilindro, densidad_cilindro
+from calculos import area_cilindro, resistencia_compresion, evolucion_resistencia , volumen_cilindro, densidad_cilindro
+from factores_ld import factor_ld
 
+# ========================================
+# BARRA LATERAL PRESENTACIÓN
+# ========================================
 
+linkedin = "https://www.linkedin.com/in/danielorlando-ramirez/" 
+email = "ingdanielrayez@gmail.com"
+
+st.sidebar.markdown("---")
+
+st.sidebar.markdown(
+    f"""
+    <div style='text-align: center; font-size: 12px; color: #888888; line-height: 1.6;'> 
+        Desarrollado por<br>
+        <strong>Daniel Ramírez</strong><br>
+        Ingeniero Civil | Desarrollo de Soluciones Analíticas<br><br>
+        <a href="{linkedin}" target="_blank" style="color: #888888; text-decoration: none;">
+            LinkedIn
+        </a> |
+        <a href="mailto:{email}" style="color: #888888; text-decoration: none;">
+            Email
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 # ========================================
 # ESTILOS CSS
 # ========================================
-#fondo gris muy claro
 
 css = """
 <style>
     .stApp {
-        background-color: #fafafa;
+        background-color: #fafafa; /*gris claro*/
     }
     a.anchor-link {
         display: none !important;
@@ -33,20 +57,8 @@ st.set_page_config(page_title="Laboratorio Concreto", layout="wide")
 st.title("Ensayo de Compresión de Cilindros de Concreto")
 
 # ========================================
-# DEFINICIONES DE NORMAS Y TIPOS DE FALLA
+#  AUXILIARES
 # ========================================
-# NORMAS_COMPRESION = {
-    # "🇨🇴 NTC 673 (Colombia)": "NTC 673",
-    # "🇻🇪 COVENIN 338 (Venezuela)": "COVENIN 338",
-    # "🇺🇸 ASTM C39 (USA)": "ASTM C39",
-    # "🇧🇷 NBR 5739 (Brasil)": "NBR 5739",
-    # "🇲🇽 NMX-C-083 (México)": "NMX-C-083",
-    # "🇦🇷 IRAM 1546 (Argentina)": "IRAM 1546",
-    # "🇵🇪 NTP 339.034 (Perú)": "NTP 339.034",
-    # "🇨🇱 NCh 1010 (Chile)": "NCh 1010",
-    # # "🇧🇴 NB 1225 (Bolivia)": "NB 1225",
-    # "🇪🇨 NEN 1015 (Ecuador)": "NEN 1015",
-# }
 
 TIPOS_FALLA = [
     "Tipo 1: conos bien formados en ambos extremos",
@@ -57,20 +69,9 @@ TIPOS_FALLA = [
     "Tipo 6: extremo puntiagudo",
 ]
 
-# IMAGENES_FALLA = {
-#     "Falla por compresión": "https://www.sioingenieria.com/portal/shared/rs.php?rsid=2193",
-#     "Falla por cortante": "https://imgur.com/dkamL7p",
-#     "Falla por tensión diagonal": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Diagonal_tension_failure.jpg/400px-Diagonal_tension_failure.jpg",
-#     "Grietas radiales": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Radial_cracks.jpg/400px-Radial_cracks.jpg",
-#     "Fractura cónica": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Conical_fracture.jpg/400px-Conical_fracture.jpg",
-#     "Falla por desprendimiento": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Spalling_failure.jpg/400px-Spalling_failure.jpg",
-#     "Falla frágil": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Brittle_failure.jpg/400px-Brittle_failure.jpg",
-#     "Otra": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Concrete_specimen.jpg/400px-Concrete_specimen.jpg"
-# }
-
 # Inicializar estado de sesión
 if "df_ensayos" not in st.session_state:
-    st.session_state.df_ensayos = pd.DataFrame(columns=["Muestra", "Fecha", "Norma", "Edad(d)", "Carga(kN)", "Diámetro(mm)", "Altura(mm)", "Densidad(kg/m3)", "Resistencia(MPa)", "Evolución(%)", "Tipo Falla"])
+    st.session_state.df_ensayos = pd.DataFrame(columns=["Muestra", "Fecha", "Edad(d)",  "Diámetro(mm)", "Altura(mm)", "Densidad(kg/m3)", "Carga Máxima(kN)", "Resistencia(MPa)", "Evolución(%)", "Tipo Falla"])
 
 df_ensayos = st.session_state.df_ensayos
 
@@ -82,7 +83,7 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.write("### Registro de Ensayo")
 
-with col2:
+with col2:   #Norma técnica con enlace
 
     st.write("") 
     norma = " NTC 673 (Colombia)" 
@@ -98,66 +99,76 @@ with col2:
     """,
     unsafe_allow_html=True, help="Haz clic para ver la norma NTC 673")
 
-with col3:
-    #texto con fecha del día de hoy centrado y en negrita  
+with col3:     #texto con fecha del día de hoy centrado y en negrita  
+
     fecha = date.today().strftime("%d/%m/%Y") 
     fecha_f = st.markdown(f"<h5 style='text-align: left; font-weight: bold; margin: 0; padding: 15px;'>{fecha}</h5>", unsafe_allow_html=True)
 
 col1, col2, col3 = st.columns(3)
 
-
 with col1:
     muestra = st.text_input("Nombre de la muestra", placeholder="Ej: Muestra 1")
-    dimensiones = st.selectbox("Dimensiones del cilindro", ["100x200 mm", "150x300 mm"])
-    peso = st.number_input("Peso (kg)", min_value=0.0, step=0.1)
+    dimensiones = st.selectbox("Dimensiones del cilindro", ["100x200 mm", "150x300 mm", "Otras dimensiones"], help="Selecciona las dimensiones estándar o elige 'Otras dimensiones' para ingresar medidas personalizadas")
+    peso = st.number_input("Peso (kg)", value= None , placeholder="Ej: 3.5")
 
-        
     # Asignar valores de diámetro y altura según dimensiones
     if dimensiones == "100x200 mm":
         diametro_default = 100
         altura_default = 200
-    else:
+    elif dimensiones == "150x300 mm":
         diametro_default = 150
         altura_default = 300
+    else:
+        diametro_default = 100
+        altura_default = 100
 
 with col2:
     fc = st.number_input("F'c (MPa)", min_value=15.0, step= 1.0)
     diametro = st.number_input("Diámetro (mm)", min_value=1.0, value=float(diametro_default), step=0.1)
-    carga = st.number_input("Carga (kN)", min_value=0.0, step=0.1)
+    area = area_cilindro(diametro)
+    carga = st.number_input("Carga (kN)", min_value=1.0, step=0.1)
 
-# Calcular resistencia
-if carga > 0 and diametro > 0:
-    resistencia = resistencia_compresion(carga, diametro)
-else:
-    resistencia = 0.0
 
 with col3:
-    edad = st.number_input("Edad (días)", min_value=1, max_value=28, value=7)
+
+    edad = st.selectbox("Edad (días)", ["1 (+- 0.5h)", "3 (+-2h)", "7 (+-6h)", "28 (+-20h)", "90 (48h)"])  
     altura = st.number_input("Altura (mm)", min_value=1.0, value=float(altura_default), step=0.1)
     tipo_falla = st.selectbox("Tipo de Falla", TIPOS_FALLA)
+    rel_ld = altura / diametro
+    factor_ld_value = factor_ld.loc[factor_ld["ld"] == round(rel_ld, 2), "factor"].values[0] if 1.0 <= rel_ld <= 1.99 else 1.0
+
+    # Calcular resistencia
+if dimensiones == "Otras dimensiones" and 1.0 <= rel_ld <= 1.99:
+    resistencia = resistencia_compresion(carga, diametro)
+    resistencia = round(resistencia * factor_ld_value, 2)
+    evolucion = evolucion_resistencia(fc, resistencia)
+
+elif dimensiones != "Otras dimensiones" and carga > 0 and diametro > 0:
+    resistencia = resistencia_compresion(carga, diametro)
+    evolucion = evolucion_resistencia(fc, resistencia)
+else:
+    st.error("La relación L/D debe estar entre 1.00 y 2.00 para cilindros de otras dimensiones.")
 
 # Calcular densidad
+if peso == None:
+    peso = 0.0
 if diametro > 0 and altura > 0:
     volumen = volumen_cilindro(diametro, altura)
-    densidad = densidad_cilindro(volumen, peso)
+    densidad = densidad_cilindro(volumen, float(peso))
 else:
     densidad = 0.0
 
-# Calcular evolución de resistencia
-evolucion = evolucion_resistencia(resistencia, fc)
-
 # Botón para registrar ensayo
 if st.button("Registrar Ensayo", type="primary"):
-    if carga > 0 and diametro > 0 and altura > 0:
+    if muestra and carga > 0:
         nuevo_ensayo = pd.DataFrame({
             "Muestra": [muestra],
             "Fecha": [fecha],       
-            "Norma": [norma],
-            "Edad(d)": [edad],
-            "Carga(kN)": [carga],
+            "Edad(d)": [edad.split(" ")[0]],
             "Diámetro(mm)": [diametro],
             "Altura(mm)": [altura],
-            "Densidad(kg/m3)": [densidad],
+            "Densidad(kg/m3)": [densidad],            
+            "Carga Máxima(kN)": [carga],
             "Resistencia(MPa)": [resistencia],
             "Evolución(%)": [evolucion],
             "Tipo Falla": [tipo_falla[:6]],
@@ -166,7 +177,7 @@ if st.button("Registrar Ensayo", type="primary"):
         st.session_state.df_ensayos = pd.concat([st.session_state.df_ensayos, nuevo_ensayo], ignore_index=True)
         st.success("Ensayo registrado correctamente")
     else:
-        st.error("Por favor completa todos los campos obligatorios: muestra, carga, diámetro y altura")
+        st.error("Completa todos los campos obligatorios: muestra y carga")
 
 # ========================================
 # SECCIÓN 2: TABLA DE RESULTADOS
@@ -177,32 +188,8 @@ st.markdown("### Resultados", help="para descargar resultados en formato csv use
 if not st.session_state.df_ensayos.empty:
     # Mostrar tabla
     df_display = st.session_state.df_ensayos.copy()
-    # df_display["Fecha"] = pd.to_datetime(df_display["Fecha"]).dt.strftime("%d/%m/%Y")
-    st.dataframe(df_display, use_container_width=True, hide_index=True)
+    st.dataframe(df_display, width= "content", hide_index=True)
     
 else:
     st.info("No se han registrado ensayos aún. Completa el formulario para agregar resultados.")
 
-
-
-linkedin = "https://www.linkedin.com/in/danielorlando-ramirez/" 
-email = "ingdanielrayez@gmail.com"
-
-st.sidebar.markdown("---")
-
-st.sidebar.markdown(
-    f"""
-    <div style='text-align: center; font-size: 12px; color: #888888; line-height: 1.6;'>
-        Desarrollado por<br>
-        <strong>Daniel Ramírez</strong><br>
-        Ingeniero Civil | Desarrollo de Soluciones Analíticas<br><br>
-        <a href="{linkedin}" target="_blank" style="color: #888888; text-decoration: none;">
-            LinkedIn
-        </a> |
-        <a href="mailto:{email}" style="color: #888888; text-decoration: none;">
-            Email
-        </a>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
